af19b4a4f5676c2ca8cbd97cd7ddf67c71a5bee8
Charles Okonkwo <ookonkwo@appzonegroup.com>
2021-02-27 21:27:26 +0100

    added user repository

---
+++ src/main/java/com/arc/userapi/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.arc.userapi.repository;
+
+import com.arc.userapi.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author Charles on 27/02/2021
+ */
+public interface UserRepository extends JpaRepository<User, Long>  {
+}
