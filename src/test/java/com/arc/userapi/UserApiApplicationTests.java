d81c058dea1c453286a5a01f728b06c89850c832
Charles Okonkwo <ookonkwo@appzonegroup.com>
2021-03-09 20:27:58 +0100

    updated pom to add run goal

--- src/test/java/com/arc/userapi/UserApiApplicationTests.java
+++ src/test/java/com/arc/userapi/UserApiApplicationTests.java
@@ -3,11 +3,11 @@ package com.arc.userapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class UserApiApplicationTests {
 
-    @Test
+    /*@Test
     void contextLoads() {
-    }
+    }*/
 
 }
